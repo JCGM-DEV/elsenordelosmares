01330b12e4d9ffa91e415161427019944d54728a
Carlos Garrido <carlos.garridomolina@gmail.com>
2026-04-04 20:17:53 +0200

    fix: add missing cinematic CSS and update service worker cache to v2

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,21 @@
-const CACHE_NAME = 'elsenormares-v1';
+const CACHE_NAME = 'elsenormares-v2';
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(['./']))
   );
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(
